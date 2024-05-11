0a596968e32c6a428cd5b982dca5c1c05d708989
Edvard Granheim Harbo <edvargh@stud.ntnu.no>
2024-05-11 21:13:58 +0200

    added dist directory and app.use(express.static('dist'))

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan('tiny'))
 
 let persons = [
@@ -86,7 +89,7 @@ let persons = [
     response.json(person)
   })
   
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
