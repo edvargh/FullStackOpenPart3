e82d41f4662ae49bb30343303f8571957f26671e
Edvard Granheim Harbo <edvargh@stud.ntnu.no>
2024-05-12 18:24:07 +0200

    did exercises 3.13 and 3.14

--- index.js
+++ index.js
@@ -1,65 +1,49 @@
+require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
+const Person = require('./models/person')
 const app = express()
 
-app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
-app.use(morgan('tiny'))
 
-let persons = [
-    {
-      id: 1,
-      name: "Arto Hellas",
-      number: "040-123456"
-    },
-    {
-      id: 2,
-      name: "Ada Lovelace",
-      number: "39-44-5323523"
-    },
-    {
-      id: 3,
-      name: "Dan Abramov",
-      number: "12-43-234345"
-    },
-    {
-      id: 4,
-      name: "Mary Poppendieck",
-      number: "39-23-6423122"
-    }
-  ]
 
-  app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-  })
 
-  app.get('/info', (request, response) => {
+  app.get('/info', async (request, response) => {
     const date = new Date();
-    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
-  })
+      try {
+          const count = await Person.countDocuments({});
+          response.send(`<p>Phonebook has info for ${count} people</p><p>${date}</p>`);
+      } catch (err) {
+          console.error(err);
+          response.status(500).json({ error: 'internal server error' });
+      }
+    });
 
   app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(people => {
+      response.json(people)
+    })
   })
 
   app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        response.json(person)
-      } else {
-        response.status(404).end()
-      }
+    Person.findById(request.params.id).then(person => {
+      response.json(person)
+    })
   })
 
   app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+    Person.deleteOne({ _id: request.params.id })
+      .then(result => {
+        if (result.deletedCount > 0) {
+          response.status(204).end()
+        } else {
+          response.status(404).json({ error: 'person not found' })
+        }
+      })
+      .catch(error => {
+        console.error(error)
+        response.status(500).json({ error: 'internal server error' })
+      })
   })
 
   app.post('/api/persons', (request, response) => {
@@ -71,25 +55,18 @@ let persons = [
       })
     }
   
-    const existingPerson = persons.find(person => person.name === body.name)
-    if (existingPerson) {
-      return response.status(400).json({ 
-        error: 'name must be unique' 
-      })
-    }
-  
-    const person = {
+    const person = new Person({
       name: body.name,
       number: body.number,
-      id: Math.floor(Math.random() * 1000000), 
-    }
-  
-    persons = persons.concat(person)
-  
-    response.json(person)
+    })
+
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+  })
+
   })
   
-  const PORT = process.env.PORT || 3001
+  const PORT = process.env.PORT
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
